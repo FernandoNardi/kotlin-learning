bce46b7a6c3f53d4c67fdeb019ac1a7ae557d12a
Nardi <fernandonardi87@gmail.com>
2017-09-25 22:08:36 -0300

    Kotlin's Testing, Exercise - Count vowels and consonants. Udemy course

---
+++ src/exercises/VowelConsonant.kt
@@ -0,0 +1,41 @@
+package exercises
+
+fun countVowels(phrase: String): Int {
+    val vowels: Array<String> = arrayOf("a", "e", "i", "o", "u")
+    var count = 0
+    for (char in phrase) {
+        if (vowels.contains(char.toLowerCase().toString())) {
+            count++
+        }
+    }
+    return count
+}
+
+fun countVowels2(phrase: String): Int {
+    val vowels = "aeiou"
+    var count = 0
+    for(letter in phrase) {
+        if(letter.toLowerCase() in vowels) count++
+    }
+    return count
+}
+
+fun countConsonants(phrase: String): Int {
+    val vowels: Array<String> = arrayOf("a", "e", "i", "o", "u")
+    var count = 0
+    for (char in phrase) {
+        if (!vowels.contains(char.toString().toLowerCase()) && char.isLetter()) {
+            count++
+        }
+    }
+    return count
+}
+
+fun countConsonants2(phrase: String): Int {
+    val consonants = "qwrtypsdfghjklçzxcvbnm"
+    var count = 0
+    for(letter in phrase) {
+        if(letter.toLowerCase() in consonants) count++
+    }
+    return count
+}
---
+++ src/exercises/VowelConsonantTest.kt
@@ -0,0 +1,26 @@
+package exercises
+
+import org.junit.Assert
+import org.junit.Test
+
+class VowelConsonantTest {
+
+    @Test
+    fun countVowels() {
+        Assert.assertEquals(8, countVowels("How many vowels do I have?"))
+        Assert.assertEquals(8, countVowels2("How many vowels do I have?"))
+    }
+
+    @Test
+    fun countConsonants() {
+        Assert.assertEquals(15, countConsonants("How many consonants do I have?"))
+        Assert.assertEquals(15, countConsonants2("How many consonants do I have?"))
+    }
+
+    @Test
+    fun countVowelsAndConsonants() {
+        val phrase = "How many vowels and consonants do I have"
+        Assert.assertEquals(12, countVowels(phrase))
+        Assert.assertEquals(21, countConsonants(phrase))
+    }
+}
