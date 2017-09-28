55570c79de77b036d638f716101083a98c2d14a6
Nardi <fernandonardi87@gmail.com>
2017-09-28 17:50:59 -0300

    Kotlin's Testing, n parameters. Udemy course

--- src/moreFunctions/MoreParameters.kt
+++ src/moreFunctions/MoreParameters.kt
@@ -5,9 +5,22 @@ fun writeAddress(street: String, city: String = "city default", state: String, c
     println("City: $city, $state - $cep")
 }
 
+fun <T> manyParameters(vararg name: T) {
+    println("There are ${name.size} parameters")
+    for (x in name) {
+        if (x is Int) {
+            println("I am int: $x")
+        } else if (x is String) {
+            println("I am string: $x")
+        }
+    }
+}
+
 fun main(args: Array<String>) {
     writeAddress(street = "1 - Raja", state = "Minas Gerais")
     writeAddress("2 - Raja", "Belo Horizonte", "Minas Gerais", "3256-897")
     writeAddress("3 - Raja", "Belo Horizonte", "Minas Gerais")
     writeAddress("4 - Raja", state = "Minas Gerais", cep = "3256-897")
+
+    manyParameters(1, 2, 3, "a", "e")
 }
